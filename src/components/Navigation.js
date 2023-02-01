dda32ff70dc104e38f3e215cd19413dc4b112eca
Melisa Z <melzahirovic@gmail.com>
2023-01-31 23:32:55 -0600

    setup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Navigation(props) {
-  const tabs = ["About", "Portfolio", "Contact", "Resume"];
+  const tabs = ["About Me", "Portfolio", "Contact", "Resume"];
   return (
     <div className="tabs is-centered">
       <ul className="nav nav-tabs">
