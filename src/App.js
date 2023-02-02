80c9b08f5f75b0fedd1714bd48610058a8e47c88
Melisa Z <melzahirovic@gmail.com>
2023-02-01 20:58:06 -0600

    more setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <About></About>
       <Footer></Footer>
     </div>
   );
